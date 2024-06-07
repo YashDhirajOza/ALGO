2e72079f8c381e842058cf071bd645226373fbc0
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-06-07 11:59:57 +0530

    Time: 34 ms (90.84%) | Memory: 16.6 MB (92.96%) - LeetSync

--- 968-beautiful-array/beautiful-array.py
+++ 968-beautiful-array/beautiful-array.py
@@ -3,7 +3,6 @@ class Solution:
         def construct_beautiful_array(nums: List[int]) -> List[int]:
             if len(nums) <= 1:
                 return nums
-            # Divide the array into two parts: odd-indexed and even-indexed elements
             odd = construct_beautiful_array(nums[::2])
             even = construct_beautiful_array(nums[1::2])
             return odd + even
