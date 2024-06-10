4638cdbef1b291ce481e714822c1ca6572b68adf
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-06-11 00:04:33 +0530

    Time: 48 ms (9.70%) | Memory: 16.3 MB (97.08%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,4 +1,4 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        exp = sorted(heights)
-        return sum(1 for i in range(len(heights)) if heights[i] != exp[i])
+        expected = sorted(heights)
+        return sum(1 for h, e in zip(heights, expected) if h != e)
