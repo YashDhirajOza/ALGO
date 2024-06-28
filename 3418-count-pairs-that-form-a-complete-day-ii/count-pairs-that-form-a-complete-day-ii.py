3b701bd1e6af6131a0f544a4957a9e7bc64eeed0
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-06-28 21:45:15 +0530

    Time: 1023 ms (30.27%) | Memory: 68.1 MB (94.32%) - LeetSync

--- 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.py
+++ 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.py
@@ -2,18 +2,14 @@ class Solution:
     def countCompleteDayPairs(self, hours: List[int]) -> int:
         remainder_count = {}
         count_pairs = 0
-        
         for hour in hours:
             remainder = hour % 24
             complement = (24 - remainder) % 24  
-            
             if complement in remainder_count:
                 count_pairs += remainder_count[complement]
-            
             if remainder in remainder_count:
                 remainder_count[remainder] += 1
             else:
                 remainder_count[remainder] = 1
-        
         return count_pairs
 
