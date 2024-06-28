2b886acd864815afd01099c2cfd1a8e5762195bf
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-06-28 20:23:48 +0530

    Time: 52 ms (61.39%) | Memory: 16.9 MB (88.65%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,19 +1,29 @@
 import itertools as it
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def is_solution(perm):
-            for (i1, i2) in it.combinations(range(len(perm)), 2):
-                if abs(i1 - i2) == abs(perm[i1] - perm[i2]):
+        def can_be_extended_to_solution(perm):
+            i = len(perm) - 1
+            for j in range(i):
+                if i - j == abs(perm[i] - perm[j]):
                     return False
             return True
         
-        solutions = []
-        
-        for perm in it.permutations(range(n)):
-            if is_solution(perm):
+        def extend(perm, n):
+            if len(perm) == n:
                 board = []
                 for row in perm:
                     board.append('.' * row + 'Q' + '.' * (n - row - 1))
                 solutions.append(board)
+                return
+            
+            for k in range(n):
+                if k not in perm:
+                    perm.append(k)
+                    if can_be_extended_to_solution(perm):
+                        extend(perm, n)
+                    perm.pop()
+        
+        solutions = []
+        extend([], n)
         
         return solutions
