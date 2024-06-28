989d388807c93451a89d20afed56516b54b58011
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-06-28 20:06:31 +0530

    Time: 321 ms (5.01%) | Memory: 17.1 MB (26.57%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        import itertools as it
+        
+        def is_solution(perm):
+            for (i1, i2) in it.combinations(range(len(perm)), 2):
+                if abs(i1 - i2) == abs(perm[i1] - perm[i2]):
+                    return False
+            return True
+        
+        solutions = []
+        
+        for perm in it.permutations(range(n)):
+            if is_solution(perm):
+                board = []
+                for row in perm:
+                    board.append('.' * row + 'Q' + '.' * (n - row - 1))
+                solutions.append(board)
+        
+        return solutions
