1a817d139769b2ca75d66a1840593ccc074b440e
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-06-30 01:35:29 +0530

    Time: 33 ms (85.97%) | Memory: 16.4 MB (68.85%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -1,11 +1,7 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        def convert_to_number(s: str) -> int:
-            numeric_str = ''.join(str(ord(char) - ord('a') + 1) for char in s)
-            return int(numeric_str)
-        def sum_of_digits(num: int) -> int:
-            return sum(int(digit) for digit in str(num))
-        number = convert_to_number(s)
-        for _ in range(k):
-            number = sum_of_digits(number)
+        number = sum(int(digit) for char in s for digit in str(ord(char) - ord('a') + 1))
+        for _ in range(k - 1):
+            number = sum(int(digit) for digit in str(number))
+        
         return number
