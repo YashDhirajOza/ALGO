32f9242bb649d488cf47740f7a34b3eee358bd22
Damm Bro <144452060+YashDhirajOza@users.noreply.github.com>
2024-10-25 23:08:17 +0530

    Create Bhen Ke Lode.py

---
+++ Bhen Ke Lode.py
@@ -0,0 +1,22 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split())) 
+    sum_even = -1
+    sum_odd = -1
+    counter_even = 0
+    counter_odd = 0
+    
+    for i in range(n):
+        if i % 2 == 0: 
+            counter_even += 1
+            sum_even = max(sum_even, arr[i])
+        else:  # Odd index
+            counter_odd += 1
+            sum_odd = max(sum_odd, arr[i])
+    
+    answer = max(sum_even, sum_odd)
+    if answer == sum_even:
+        print(answer + counter_even)
+    else:
+        print(answer + counter_odd)
